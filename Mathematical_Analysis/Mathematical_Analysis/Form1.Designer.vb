48d70527b3a72f033e495a4ac6dfacb113ed9872
Thinzar Kyaw <164594037+lil-ayla@users.noreply.github.com>
2024-04-19 12:05:33 +0630

    Add files via upload

---
+++ Mathematical_Analysis/Mathematical_Analysis/Form1.Designer.vb
@@ -0,0 +1,317 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmdisplay
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnupload = New System.Windows.Forms.Button()
+        Me.txtupload = New System.Windows.Forms.TextBox()
+        Me.txtsolution = New System.Windows.Forms.TextBox()
+        Me.lblDescription = New System.Windows.Forms.Label()
+        Me.btnreport = New System.Windows.Forms.Button()
+        Me.lblDecimal = New System.Windows.Forms.Label()
+        Me.chkimport = New System.Windows.Forms.CheckBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.btnaccess = New System.Windows.Forms.Button()
+        Me.btncarry = New System.Windows.Forms.Button()
+        Me.txtimport = New System.Windows.Forms.TextBox()
+        Me.cboDecimalPlace = New System.Windows.Forms.ComboBox()
+        Me.chkMean = New System.Windows.Forms.CheckBox()
+        Me.chkMedian = New System.Windows.Forms.CheckBox()
+        Me.chkMode = New System.Windows.Forms.CheckBox()
+        Me.chkCC = New System.Windows.Forms.CheckBox()
+        Me.btndetermine = New System.Windows.Forms.Button()
+        Me.txtreport = New System.Windows.Forms.TextBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'btnupload
+        '
+        Me.btnupload.Location = New System.Drawing.Point(10, 35)
+        Me.btnupload.Margin = New System.Windows.Forms.Padding(4)
+        Me.btnupload.Name = "btnupload"
+        Me.btnupload.Size = New System.Drawing.Size(207, 50)
+        Me.btnupload.TabIndex = 0
+        Me.btnupload.Text = "Upload Text File"
+        Me.btnupload.UseVisualStyleBackColor = True
+        '
+        'txtupload
+        '
+        Me.txtupload.BackColor = System.Drawing.Color.White
+        Me.txtupload.Enabled = False
+        Me.txtupload.Location = New System.Drawing.Point(225, 43)
+        Me.txtupload.Margin = New System.Windows.Forms.Padding(4)
+        Me.txtupload.Name = "txtupload"
+        Me.txtupload.Size = New System.Drawing.Size(347, 34)
+        Me.txtupload.TabIndex = 1
+        '
+        'txtsolution
+        '
+        Me.txtsolution.Font = New System.Drawing.Font("Yu Gothic UI", 16.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtsolution.ForeColor = System.Drawing.Color.FromArgb(CType(CType(166, Byte), Integer), CType(CType(27, Byte), Integer), CType(CType(82, Byte), Integer))
+        Me.txtsolution.Location = New System.Drawing.Point(53, 401)
+        Me.txtsolution.Margin = New System.Windows.Forms.Padding(4)
+        Me.txtsolution.Multiline = True
+        Me.txtsolution.Name = "txtsolution"
+        Me.txtsolution.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.txtsolution.Size = New System.Drawing.Size(779, 279)
+        Me.txtsolution.TabIndex = 2
+        '
+        'lblDescription
+        '
+        Me.lblDescription.AutoSize = True
+        Me.lblDescription.BackColor = System.Drawing.Color.FromArgb(CType(CType(252, Byte), Integer), CType(CType(222, Byte), Integer), CType(CType(231, Byte), Integer))
+        Me.lblDescription.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblDescription.ForeColor = System.Drawing.Color.FromArgb(CType(CType(166, Byte), Integer), CType(CType(27, Byte), Integer), CType(CType(82, Byte), Integer))
+        Me.lblDescription.Location = New System.Drawing.Point(53, 367)
+        Me.lblDescription.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lblDescription.Name = "lblDescription"
+        Me.lblDescription.Size = New System.Drawing.Size(159, 30)
+        Me.lblDescription.TabIndex = 3
+        Me.lblDescription.Text = "Solution/Report"
+        '
+        'btnreport
+        '
+        Me.btnreport.Enabled = False
+        Me.btnreport.Location = New System.Drawing.Point(580, 93)
+        Me.btnreport.Margin = New System.Windows.Forms.Padding(4)
+        Me.btnreport.Name = "btnreport"
+        Me.btnreport.Size = New System.Drawing.Size(258, 50)
+        Me.btnreport.TabIndex = 8
+        Me.btnreport.Text = "Report"
+        Me.btnreport.UseVisualStyleBackColor = True
+        '
+        'lblDecimal
+        '
+        Me.lblDecimal.AutoSize = True
+        Me.lblDecimal.BackColor = System.Drawing.Color.White
+        Me.lblDecimal.Location = New System.Drawing.Point(329, 50)
+        Me.lblDecimal.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lblDecimal.Name = "lblDecimal"
+        Me.lblDecimal.Size = New System.Drawing.Size(151, 28)
+        Me.lblDecimal.TabIndex = 9
+        Me.lblDecimal.Text = "Decimal Places:"
+        '
+        'chkimport
+        '
+        Me.chkimport.AutoSize = True
+        Me.chkimport.Location = New System.Drawing.Point(29, 111)
+        Me.chkimport.Margin = New System.Windows.Forms.Padding(4)
+        Me.chkimport.Name = "chkimport"
+        Me.chkimport.Size = New System.Drawing.Size(147, 32)
+        Me.chkimport.TabIndex = 11
+        Me.chkimport.Text = "Import Data"
+        Me.chkimport.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.BackColor = System.Drawing.Color.FromArgb(CType(CType(252, Byte), Integer), CType(CType(222, Byte), Integer), CType(CType(231, Byte), Integer))
+        Me.GroupBox1.Controls.Add(Me.btnaccess)
+        Me.GroupBox1.Controls.Add(Me.btncarry)
+        Me.GroupBox1.Controls.Add(Me.txtimport)
+        Me.GroupBox1.Controls.Add(Me.chkimport)
+        Me.GroupBox1.Controls.Add(Me.btnupload)
+        Me.GroupBox1.Controls.Add(Me.txtupload)
+        Me.GroupBox1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(166, Byte), Integer), CType(CType(27, Byte), Integer), CType(CType(82, Byte), Integer))
+        Me.GroupBox1.Location = New System.Drawing.Point(24, 13)
+        Me.GroupBox1.Margin = New System.Windows.Forms.Padding(4)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Padding = New System.Windows.Forms.Padding(4)
+        Me.GroupBox1.Size = New System.Drawing.Size(864, 175)
+        Me.GroupBox1.TabIndex = 12
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Data Input "
+        '
+        'btnaccess
+        '
+        Me.btnaccess.Enabled = False
+        Me.btnaccess.Location = New System.Drawing.Point(580, 101)
+        Me.btnaccess.Margin = New System.Windows.Forms.Padding(4)
+        Me.btnaccess.Name = "btnaccess"
+        Me.btnaccess.Size = New System.Drawing.Size(258, 50)
+        Me.btnaccess.TabIndex = 14
+        Me.btnaccess.Text = "Access Data"
+        Me.btnaccess.UseVisualStyleBackColor = True
+        '
+        'btncarry
+        '
+        Me.btncarry.Enabled = False
+        Me.btncarry.Location = New System.Drawing.Point(580, 43)
+        Me.btncarry.Margin = New System.Windows.Forms.Padding(4)
+        Me.btncarry.Name = "btncarry"
+        Me.btncarry.Size = New System.Drawing.Size(258, 49)
+        Me.btncarry.TabIndex = 13
+        Me.btncarry.Text = "Carry Data"
+        Me.btncarry.UseVisualStyleBackColor = True
+        '
+        'txtimport
+        '
+        Me.txtimport.BackColor = System.Drawing.Color.White
+        Me.txtimport.Enabled = False
+        Me.txtimport.ForeColor = System.Drawing.Color.FromArgb(CType(CType(166, Byte), Integer), CType(CType(27, Byte), Integer), CType(CType(82, Byte), Integer))
+        Me.txtimport.Location = New System.Drawing.Point(225, 109)
+        Me.txtimport.Margin = New System.Windows.Forms.Padding(4)
+        Me.txtimport.Name = "txtimport"
+        Me.txtimport.Size = New System.Drawing.Size(346, 34)
+        Me.txtimport.TabIndex = 12
+        '
+        'cboDecimalPlace
+        '
+        Me.cboDecimalPlace.ForeColor = System.Drawing.Color.FromArgb(CType(CType(166, Byte), Integer), CType(CType(27, Byte), Integer), CType(CType(82, Byte), Integer))
+        Me.cboDecimalPlace.FormattingEnabled = True
+        Me.cboDecimalPlace.Items.AddRange(New Object() {"1", "2", "3", "4", "5"})
+        Me.cboDecimalPlace.Location = New System.Drawing.Point(488, 47)
+        Me.cboDecimalPlace.Margin = New System.Windows.Forms.Padding(4)
+        Me.cboDecimalPlace.Name = "cboDecimalPlace"
+        Me.cboDecimalPlace.Size = New System.Drawing.Size(59, 36)
+        Me.cboDecimalPlace.TabIndex = 13
+        Me.cboDecimalPlace.Text = "3"
+        '
+        'chkMean
+        '
+        Me.chkMean.AutoSize = True
+        Me.chkMean.Location = New System.Drawing.Point(29, 49)
+        Me.chkMean.Margin = New System.Windows.Forms.Padding(4)
+        Me.chkMean.Name = "chkMean"
+        Me.chkMean.Size = New System.Drawing.Size(89, 32)
+        Me.chkMean.TabIndex = 14
+        Me.chkMean.Text = "Mean"
+        Me.chkMean.UseVisualStyleBackColor = True
+        '
+        'chkMedian
+        '
+        Me.chkMedian.AutoSize = True
+        Me.chkMedian.Location = New System.Drawing.Point(126, 49)
+        Me.chkMedian.Margin = New System.Windows.Forms.Padding(4)
+        Me.chkMedian.Name = "chkMedian"
+        Me.chkMedian.Size = New System.Drawing.Size(106, 32)
+        Me.chkMedian.TabIndex = 15
+        Me.chkMedian.Text = "Median"
+        Me.chkMedian.UseVisualStyleBackColor = True
+        '
+        'chkMode
+        '
+        Me.chkMode.AutoSize = True
+        Me.chkMode.Location = New System.Drawing.Point(240, 49)
+        Me.chkMode.Margin = New System.Windows.Forms.Padding(4)
+        Me.chkMode.Name = "chkMode"
+        Me.chkMode.Size = New System.Drawing.Size(91, 32)
+        Me.chkMode.TabIndex = 16
+        Me.chkMode.Text = "Mode"
+        Me.chkMode.UseVisualStyleBackColor = True
+        '
+        'chkCC
+        '
+        Me.chkCC.AutoSize = True
+        Me.chkCC.Location = New System.Drawing.Point(29, 103)
+        Me.chkCC.Margin = New System.Windows.Forms.Padding(4)
+        Me.chkCC.Name = "chkCC"
+        Me.chkCC.Size = New System.Drawing.Size(241, 32)
+        Me.chkCC.TabIndex = 17
+        Me.chkCC.Text = "Correlation Coefficient"
+        Me.chkCC.UseVisualStyleBackColor = True
+        '
+        'btndetermine
+        '
+        Me.btndetermine.Enabled = False
+        Me.btndetermine.Location = New System.Drawing.Point(580, 36)
+        Me.btndetermine.Margin = New System.Windows.Forms.Padding(4)
+        Me.btndetermine.Name = "btndetermine"
+        Me.btndetermine.Size = New System.Drawing.Size(259, 49)
+        Me.btndetermine.TabIndex = 18
+        Me.btndetermine.Text = "Determine"
+        Me.btndetermine.UseVisualStyleBackColor = True
+        '
+        'txtreport
+        '
+        Me.txtreport.Enabled = False
+        Me.txtreport.Location = New System.Drawing.Point(278, 101)
+        Me.txtreport.Margin = New System.Windows.Forms.Padding(4)
+        Me.txtreport.Name = "txtreport"
+        Me.txtreport.Size = New System.Drawing.Size(294, 34)
+        Me.txtreport.TabIndex = 19
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.BackColor = System.Drawing.Color.FromArgb(CType(CType(252, Byte), Integer), CType(CType(222, Byte), Integer), CType(CType(231, Byte), Integer))
+        Me.GroupBox2.Controls.Add(Me.lblDecimal)
+        Me.GroupBox2.Controls.Add(Me.txtreport)
+        Me.GroupBox2.Controls.Add(Me.btnreport)
+        Me.GroupBox2.Controls.Add(Me.cboDecimalPlace)
+        Me.GroupBox2.Controls.Add(Me.btndetermine)
+        Me.GroupBox2.Controls.Add(Me.chkMean)
+        Me.GroupBox2.Controls.Add(Me.chkCC)
+        Me.GroupBox2.Controls.Add(Me.chkMedian)
+        Me.GroupBox2.Controls.Add(Me.chkMode)
+        Me.GroupBox2.ForeColor = System.Drawing.Color.FromArgb(CType(CType(166, Byte), Integer), CType(CType(27, Byte), Integer), CType(CType(82, Byte), Integer))
+        Me.GroupBox2.Location = New System.Drawing.Point(24, 207)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(864, 157)
+        Me.GroupBox2.TabIndex = 20
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Options"
+        '
+        'frmdisplay
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(11.0!, 28.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(250, Byte), Integer), CType(CType(180, Byte), Integer), CType(CType(200, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(916, 693)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.lblDescription)
+        Me.Controls.Add(Me.txtsolution)
+        Me.Font = New System.Drawing.Font("Yu Gothic UI Semibold", 10.0!, System.Drawing.FontStyle.Bold)
+        Me.Margin = New System.Windows.Forms.Padding(4)
+        Me.Name = "frmdisplay"
+        Me.Text = "Mathematical Analysis"
+        Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnupload As Button
+    Friend WithEvents txtupload As TextBox
+    Friend WithEvents txtsolution As TextBox
+    Friend WithEvents lblDescription As Label
+    Friend WithEvents btnreport As Button
+    Friend WithEvents lblDecimal As Label
+    Friend WithEvents chkimport As CheckBox
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents txtimport As TextBox
+    Friend WithEvents btncarry As Button
+    Friend WithEvents btnaccess As Button
+    Friend WithEvents cboDecimalPlace As ComboBox
+    Friend WithEvents chkMean As CheckBox
+    Friend WithEvents chkMedian As CheckBox
+    Friend WithEvents chkMode As CheckBox
+    Friend WithEvents chkCC As CheckBox
+    Friend WithEvents btndetermine As Button
+    Friend WithEvents txtreport As TextBox
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+End Class
---
+++ Mathematical_Analysis/Mathematical_Analysis/Form1.vb
@@ -0,0 +1,345 @@
+﻿Public Class frmdisplay
+    Dim strFile As String
+    Dim strvalue As String
+    Dim outcome As String
+    Dim dcmp As Integer
+    Dim ccoutcome As Double
+    Dim cc As Boolean = False
+
+    Function Mean_Answer(facts As Integer()) As Double
+        Dim summation As Integer = 0
+        Dim xbar As Double
+        For itg As Integer = 0 To facts.Length - 1
+            summation += facts(itg)
+        Next
+        xbar = Convert.ToDouble(summation / (facts.Length - 1))
+        Return xbar
+    End Function
+
+    Function Median_Answer(facts As Integer()) As Double
+        If facts.Length > 0 Then
+            ' Sort the array
+            Array.Sort(facts)
+
+            ' Check if the array length is even or odd
+            If facts.Length Mod 2 = 0 Then
+                ' If it is even, average the two central facts
+                Dim centre1 As Integer = facts.Length \ 2
+                Dim centre2 As Integer = centre1 - 1
+                Return (facts(centre1) + facts(centre2)) / 2.0
+            Else
+                ' If odd, return the central fact
+                Dim centre As Integer = facts.Length \ 2
+                Return facts(centre)
+            End If
+        Else
+            Return 0
+        End If
+
+    End Function
+
+    Function Mode_Answer(facts As Integer()) As Integer
+        If facts.Length > 0 Then
+            ' Generate a dictionary to save each number's frequency
+            Dim FMap As New Dictionary(Of Integer, Integer)
+
+            ' Go over the array again to count frequencies
+            For Each no As Integer In facts
+                If FMap.ContainsKey(no) Then
+                    FMap(no) += 1
+                Else
+                    FMap(no) = 1
+                End If
+            Next
+
+            ' Check the maximum frequency to find the mode or modes
+            Dim maxF As Integer = FMap.Values.Max()
+            Dim modelist As New List(Of Integer)
+
+            For Each pair In FMap
+                If pair.Value = maxF Then
+                    modelist.Add(pair.Key)
+                End If
+            Next
+
+            ' Return the mode or modes
+            If modelist.Count = 1 Then
+                Return modelist(0)
+            Else
+                ' If there are several modes, you may want to manage it properly
+                ' This example returns the first mode discovered to keep it simple 
+                Return modelist(0)
+            End If
+        Else
+            Return 0
+        End If
+    End Function
+
+    Function Correlation_Coefficient_Answer(a() As Integer, b() As Integer) As Double
+        ' Check if the arrays's length are the same
+        If a.Length = b.Length AndAlso a.Length > 0 Then
+            ' Rank the set of data
+            Dim arank() As Integer = Order_Arr(a)
+            Dim brank() As Integer = Order_Arr(b)
+
+            ' Compute the gaps between orders
+            Dim rankgaps() As Integer = Minus_Arr(arank, brank)
+
+            ' Square the rank gaps 
+            Dim rankgapssquared() As Integer = CubicArr(rankgaps)
+
+            ' total of the squared order gaps
+            Dim totalofsquaredgaps As Double = rankgapssquared.Sum()
+
+            ' Compute Spearman's rank correlation coefficient
+            Dim n As Integer = a.Length
+            Dim spearmancc As Double = 1 - ((6 * totalofsquaredgaps) / (n * (n * n - 1)))
+
+            Return spearmancc
+        Else
+            ' If arrays are empty or not the equivalent length, return a value of 0
+            Return 0
+        End If
+    End Function
+
+    Function Order_Arr(arr() As Integer) As Integer()
+        ' To store ranks of data,make an array
+        Dim orders(arr.Length - 1) As Integer
+
+        ' To keep the index-value pairs,generate a list
+        Dim idxvaluepairs As New List(Of KeyValuePair(Of Integer, Integer))
+
+        'Add index-value pairs to the list
+        For itg As Integer = 0 To arr.Length - 1
+            idxvaluepairs.Add(New KeyValuePair(Of Integer, Integer)(itg, arr(itg)))
+        Next
+
+        ' Sort the list according to the values
+        idxvaluepairs.Sort(Function(x, y) x.Value.CompareTo(y.Value))
+
+        ' Assign the ranks to the values sorted
+        For itg As Integer = 0 To idxvaluepairs.Count - 1
+            orders(idxvaluepairs(itg).Key) = itg + 1
+        Next
+
+        Return orders
+    End Function
+
+    Function Minus_Arr(arr1() As Integer, arr2() As Integer) As Integer()
+        ' Check if the arrays's length are the same
+        If arr1.Length = arr2.Length Then
+            ' Generate an array to keep gaps
+            Dim gaps(arr1.Length - 1) As Integer
+
+            ' Determine the gaps between related numbers
+            For itg As Integer = 0 To arr1.Length - 1
+                gaps(itg) = arr1(itg) - arr2(itg)
+            Next
+
+            Return gaps
+        Else
+            ' if arrays are differ in length, return the blank array 
+            Return {}
+        End If
+    End Function
+
+    Function CubicArr(arr() As Integer) As Integer()
+        ' Generate an array for storing squared values
+        Dim squaredvlus(arr.Length - 1) As Integer
+
+        ' Find the square of every number
+        For itg As Integer = 0 To arr.Length - 1
+            squaredvlus(itg) = arr(itg) * arr(itg)
+        Next
+
+        Return squaredvlus
+    End Function
+
+    Sub correlationDescription(cc As Double)
+        ' Describe the direction and strength of the correlation
+        Dim direction As String = ""
+        Dim strength As String = ""
+
+        ' Show off the interpretation of the correlation
+        outcome = "Correlation coefficient of " + Convert.ToString(Math.Round(ccoutcome, dcmp)) + vbNewLine
+        If cc > 0 Then
+            direction = " positive"
+        ElseIf cc < 0 Then
+            direction = " negative"
+        Else
+            direction = ""
+        End If
+        If Math.Abs(cc) >= 0.8 Then
+            strength = "strong"
+        ElseIf Math.Abs(cc) >= 0.5 Then
+            strength = "medium"
+        ElseIf Math.Abs(cc) >= 0.3 Then
+            strength = "low"
+        Else
+            strength = "very low"
+        End If
+
+        ' Interpretation of the correlation according to the standard rules
+        If Math.Abs(cc) >= 0.8 Then
+            outcome += "This is a " + strength + direction + ",very strong correlation."
+        ElseIf Math.Abs(cc) >= 0.5 Then
+            outcome += "This is a medium correlation."
+        ElseIf Math.Abs(cc) >= 0.3 Then
+            outcome += "This is a low correlation."
+        Else
+            outcome += "This is a very low or no correlation."
+        End If
+
+        If cc > 0 Then
+            outcome += " As the value of X rises so does the value of Y."
+        ElseIf cc < 0 Then
+            outcome += " As the value of X rises but declines the value of Y."
+        End If
+        txtsolution.Text = outcome
+    End Sub
+
+    Private Sub chkimport_CheckedChanged(sender As Object, e As EventArgs) Handles chkimport.CheckedChanged
+        If chkimport.Checked Then
+            txtimport.Enabled = True
+            btnaccess.Enabled = True
+        Else
+            txtimport.Enabled = False
+            btnaccess.Enabled = False
+        End If
+    End Sub
+
+    Private Sub btnupload_Click(sender As Object, e As EventArgs) Handles btnupload.Click
+        Dim ofd As OpenFileDialog = New OpenFileDialog()
+
+        ofd.Title = "Please Choose a File for Analysis"
+        ofd.InitialDirectory = "D:\"
+        ofd.Filter = "All files (*.*)|*.*|All files (*.*)|*.*"
+        ofd.FilterIndex = 2
+        ofd.RestoreDirectory = True
+
+        If ofd.ShowDialog() = DialogResult.OK Then
+            strFile = ofd.FileName
+            txtupload.Text = strFile
+            btncarry.Enabled = True
+        End If
+    End Sub
+
+    Private Sub btncarry_Click(sender As Object, e As EventArgs) Handles btncarry.Click
+
+        If txtupload.Text = "" Then
+            MsgBox("Please Choose a Text File!")
+            Exit Sub
+        End If
+
+        Dim objectReader As New System.IO.StreamReader(strFile)
+
+        strvalue = objectReader.ReadToEnd
+        txtimport.Text = strvalue
+        'MsgBox("Select Mean, Median, Mode or Correlation Coefficient to evaluate the data.")
+        btncarry.Enabled = False
+        btndetermine.Enabled = True
+        objectReader.Close()
+        txtsolution.Text = "Carried data successfully for determining ... "
+    End Sub
+
+    Private Sub btnaccess_Click(sender As Object, e As EventArgs) Handles btnaccess.Click
+        If txtimport.Text = "" Then
+            MsgBox("Please import Data in Text Box!")
+            Exit Sub
+        End If
+        strvalue = txtimport.Text
+        'MsgBox("Select Mean, Median, Mode or Correlation Coefficient to evaluate the data.")
+        btnaccess.Enabled = False
+        chkimport.Checked = False
+        txtimport.Enabled = False
+        btndetermine.Enabled = True
+        txtsolution.Text = "Carried data successfully for determining ... "
+    End Sub
+
+    Private Sub btndetermine_Click(sender As Object, e As EventArgs) Handles btndetermine.Click
+        If chkMean.Checked = False And chkMedian.Checked = False And chkMode.Checked = False And chkCC.Checked = False Then
+            MsgBox("Please select Mean, Median, Mode, or Correlation Coefficient!")
+            Exit Sub
+        End If
+        outcome = ""
+        'Collection of data for determining mean, median and mode
+        Dim dataarr As String() = strvalue.Split(" "c)
+
+        Dim DataInt(dataarr.Length) As Integer
+
+        For itg As Integer = 0 To dataarr.Length - 1
+            If Not (IsNumeric(dataarr(itg))) Then
+                MsgBox("Please input only Integer values, separated by space: e.g.,.8 1 3 2 7")
+                Exit Sub
+            Else
+                DataInt(itg) = Convert.ToInt32(dataarr(itg))
+            End If
+        Next
+
+        dcmp = Convert.ToInt32(cboDecimalPlace.Text)
+
+        If chkMean.Checked Then
+            'Call Mean_Answer Function
+            outcome += "**************************" + vbNewLine + "Mean: " + Convert.ToString(Math.Round(Mean_Answer(DataInt), dcmp)) + vbNewLine
+        End If
+
+        If chkMedian.Checked Then
+            'Call Median_Answer Function
+            outcome += "**************************" + vbNewLine + "Median: " + Convert.ToString(Math.Round(Median_Answer(DataInt), dcmp)) + vbNewLine
+        End If
+
+        If chkMode.Checked Then
+            'Call Mode_Answer Function
+            outcome += "**************************" + vbNewLine + "Mode: " + Convert.ToString(Math.Round(Mode_Answer(DataInt), dcmp)) + vbNewLine
+        End If
+
+        If chkCC.Checked Then
+            'Data set for determining correlation coefficient
+            Dim dataarr2 As String() = txtreport.Text.Split(" "c)
+
+            Dim DataInt2(dataarr.Length) As Integer
+
+            For itg As Integer = 0 To dataarr2.Length - 1
+                If Not (IsNumeric(dataarr2(itg))) Then
+                    MsgBox("Please input integer values, separating by space: e.g.,. 3 7 9 2 8 in required text box ")
+                    Exit Sub
+                Else
+                    DataInt2(itg) = Convert.ToInt32(dataarr2(itg))
+                End If
+            Next
+
+            'Call Correlation_Coefficient_Answer Function
+            cc = True
+            ccoutcome = Correlation_Coefficient_Answer(DataInt, DataInt2)
+            outcome += "**************************" + vbNewLine + "Correlation Coefficient: " + Convert.ToString(Math.Round(ccoutcome, dcmp)) + vbNewLine
+        End If
+
+        'Display final outcome
+        txtsolution.Text = outcome
+
+
+    End Sub
+
+    Private Sub chkCC_CheckedChanged(sender As Object, e As EventArgs) Handles chkCC.CheckedChanged
+        If chkCC.Checked = True Then
+            btnreport.Enabled = True
+            txtreport.Enabled = True
+        Else
+            btnreport.Enabled = False
+            txtreport.Enabled = False
+        End If
+    End Sub
+
+    Private Sub btnreport_Click(sender As Object, e As EventArgs) Handles btnreport.Click
+        If cc Then 'Identify if correlation coefficient is calculated or not
+            correlationDescription(ccoutcome)
+        Else
+            MsgBox("Please calculate correlation coefficient first to see the report of answer")
+        End If
+
+    End Sub
+
+    Private Sub GroupBox1_Enter(sender As Object, e As EventArgs) Handles GroupBox1.Enter
+
+    End Sub
+End Class
